ff3712b5bbc2189bc0019bf364802d2cd364f0fc
jdufjfbkdhsn@gmail.com <jdufjfbkdhsn@gmail.com>
2026-05-07 20:18:52 +0900

    완성본

--- algorithm/002_12223700_KimGyuNam.cpp
+++ algorithm/002_12223700_KimGyuNam.cpp
@@ -7,10 +7,17 @@ int TotalTime = 0;
 struct Player {//record와 id를 기록할 player구조 생성
 	int id;
 	int record;
+	Player(int i, int r) {
+
+		id = i;
+
+		record = r;
+
+	}
 };
 
 //player객체를 넣는 벡터 v1,빈공간의 위치 인덱스,옮겨야할 노드 K,N은 접근할 수 있는 유효한 마지막 인덱스를 의미,isSort는 정렬에서만 바꿀수 있도록 설정
-void fixheap(vector<Player>& v1, int vacant, Player K, int N,bool isSort) {//fixheap 구현(construct heap용)
+void fixheap(vector<Player>& v1, int vacant, Player K, int N, bool isSort) {//fixheap 구현(construct heap용)
 	if (vacant * 2 > N) {    //vacant가 리프인 경우,N이 마지막 유효한 인덱스이므로 자식이 N보다 크면 리프다
 		v1[vacant] = K; //fixheap에서 vacant가 리프이므로 현재 자리에 k삽입
 	}
@@ -37,19 +44,19 @@ void fixheap(vector<Player>& v1, int vacant, Player K, int N,bool isSort) {//fix
 		else {//그렇지 않다면 smallerChild를 vacant위치로 바꿔주고,smaller child인덱스를 vacant로 설정
 			v1[vacant] = v1[smallerChild];
 			if (isSort) TotalTime += 1;//vacant의 깊이가 1칸 밑으로 이동,정렬할때만 바뀌도록 설정
-			fixheap(v1, smallerChild, K, N,isSort);//vacant의 위치를 smallerChild로 바꿔 재귀호출
+			fixheap(v1, smallerChild, K, N, isSort);//vacant의 위치를 smallerChild로 바꿔 재귀호출
 		}
 	}
 }
 
 
 //v1벡터 만들고, 루트를 표현하기위한 index,전체 노드개수 N
-void constructHeap(vector<Player>&v1,int root,int N) {
-	if (root*2 <=N) {//자식이 있는지 확인,있을때만 실행
-		constructHeap(v1, root * 2,N);//왼쪽 자식을 루트로 하는 서브트리에서 실행
-		constructHeap(v1, root * 2 + 1,N);//오른쪽 자식을 루트로 하는 서브트리에서 실행
+void constructHeap(vector<Player>& v1, int root, int N) {
+	if (root * 2 <= N) {//자식이 있는지 확인,있을때만 실행
+		constructHeap(v1, root * 2, N);//왼쪽 자식을 루트로 하는 서브트리에서 실행
+		constructHeap(v1, root * 2 + 1, N);//오른쪽 자식을 루트로 하는 서브트리에서 실행
 		Player K = v1[root];//바꿔야되는 위치의 루트를 K로 해서 fixheap에 넣어줌
-		fixheap(v1, root, K, N,false);
+		fixheap(v1, root, K, N, false);
 	}
 }
 
@@ -63,7 +70,7 @@ int getHeight(int N) {//힙의 높이 구하는 함수,이진 탐색 최악의 �
 }
 
 
-void bubbleUpHeap(vector<Player>&v1, int vacant, Player K) {//처음 이동한 vacant에서의 값을 비교
+void bubbleUpHeap(vector<Player>& v1, int vacant, Player K) {//처음 이동한 vacant에서의 값을 비교
 	if (vacant == 1) {//vacant가 루트에 위치하는 경우, vacant인덱스에 K를 넣어주고 끝
 		v1[vacant] = K;
 	}
@@ -81,9 +88,9 @@ void bubbleUpHeap(vector<Player>&v1, int vacant, Player K) {//처음 이동한 v
 }
 
 
-int promote(vector<Player>& v1, int hStop, int vacant, int h,int N) {//vacant의 위치를 한번에 낮춰주는 함수
+int promote(vector<Player>& v1, int hStop, int vacant, int h, int N) {//vacant의 위치를 한번에 낮춰주는 함수
 	int vacStop;
-	if (h <= hStop || vacant*2>N) {//목표 높이에 도달했거나 자식이 없으면 정지
+	if (h <= hStop || vacant * 2 > N) {//목표 높이에 도달했거나 자식이 없으면 정지
 		vacStop = vacant;
 	}
 	else if (vacant * 2 == N) {//왼쪽 자식만 있는경우
@@ -108,11 +115,11 @@ int promote(vector<Player>& v1, int hStop, int vacant, int h,int N) {//vacant의
 
 void fixHeapFast(vector<Player>& v1, int vacant, Player K, int h, int N) {//heapsort용 fixheapfast
 	if (h <= 1) {//높이가 1 이하인 경우,promote를 사용하더라도 어차피 한층의 두번의 비교가 필요,따라서 직접 비교로 불필요한 계산을 줄인다
-		fixheap(v1, vacant, K, N,true);
+		fixheap(v1, vacant, K, N, true);
 	}
 	else {
 		int hStop = h / 2;//한번에 내릴 목표 설정
-		int vacStop = promote(v1, hStop, vacant, h,N);//promote함수를 통해 vacStop인덱스를 구함
+		int vacStop = promote(v1, hStop, vacant, h, N);//promote함수를 통해 vacStop인덱스를 구함
 		int vacParent = vacStop / 2;
 		if (v1[vacParent].record > K.record) {//부모가 K보다 큰경우
 			v1[vacStop] = v1[vacParent];
@@ -126,14 +133,14 @@ void fixHeapFast(vector<Player>& v1, int vacant, Player K, int h, int N) {//heap
 }
 
 
-vector<int> fastheapSort(vector<Player>& v1,int N) {//heap에서 정렬을 시키는 함수
+vector<int> fastheapSort(vector<Player>& v1, int N) {//heap에서 정렬을 시키는 함수
 	vector<int> rankTable;//리턴 해줄 등수표
 	rankTable.push_back({ -1 });//등수표 인덱스 0에 더미값 삽입
 	for (int i = N;i > 1;i--) {//노드의 개수만큼 계산해줘야한다,이때 노드의 개수가 1개인 경우 바로 넣으므로 2이상일때만
 		Player curMin = v1[1];//루트 값(최솟값) 저장
 		v1[1] = v1[i];//맨 마지막 노드를 루트로 이동
 		rankTable.push_back(curMin.id);//등수표의 최솟값의 id삽입
-		int h = getHeight(i-1); //남은 노드의 높이 계산
+		int h = getHeight(i - 1); //남은 노드의 높이 계산
 		TotalTime += 1;//vacant의 깊이가 0으로 이동
 		fixHeapFast(v1, 1, v1[1], h, i - 1);
 	}
@@ -146,6 +153,8 @@ vector<int> fastheapSort(vector<Player>& v1,int N) {//heap에서 정렬을 시�
 int main() {
 	int T{ 0 };
 	cin >> T;
+	ios_base::sync_with_stdio(false);//타임 리미트를 해결하기 위해서 속도 증가
+	cin.tie(NULL);
 
 	for (int i = 0;i < T;i++) {//T번 반복
 		vector <Player> v1;//Player힙 배열을 만들 vector구성
@@ -156,7 +165,7 @@ int main() {
 		int N;
 		cin >> N;
 
-		for(int j=0;j<N;j++){//힙에 내용물 삽입,내용물 개수 설정
+		for (int j = 0;j < N;j++) {//힙에 내용물 삽입,내용물 개수 설정
 			int pid, pr;
 			cin >> pid >> pr;
 			Player player1(pid, pr);
@@ -170,7 +179,7 @@ int main() {
 		}
 
 		cout << endl;
-		vector<int> rankTable=fastheapSort(v1, N);//힙 정렬
+		vector<int> rankTable = fastheapSort(v1, N);//힙 정렬
 
 		for (int k = 1;k < N + 1;k++) {//힙 정렬후 출력
 			cout << rankTable[k] << " ";
@@ -178,7 +187,7 @@ int main() {
 
 		cout << endl;
 		cout << TotalTime << endl;
-		
+
 		v1.clear();//한번의 반복이 끝난 후 벡터 초기화
 	}
 }
